338f861ee5f5d568e2db316ffcb093cccbc0d2f4
BigCat <yzugsr@gmail.com>
2011-01-12 14:17:42 +0800

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudTest::Application.config.secret_token = 'f538c01bebd803a5c6b0be4522a96cb9c5d7e2c7bbc272e3d2feebe1c801abe5436ec840e27c5608e3c6ae9a94403e83c7a77f2323a39d226593b534f9750852'
